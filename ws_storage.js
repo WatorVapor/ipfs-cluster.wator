31e44c1bebe4eea47e154483852309b77273a176
Wator Vapor <watorvapor@gmail.com>
2018-03-20 23:48:48 +0900

    Update ws_storage.js

--- ws_storage.js
+++ ws_storage.js
@@ -8,7 +8,7 @@ wss.on('connection', function connection(ws) {
     //console.log('message: ws=<', ws,'>');
     try {
       let jsonMsg = JSON.parse(msg);
-      console.log('message: jsonMsg=<', jsonMsg,'>');
+      //console.log('message: jsonMsg=<', jsonMsg,'>');
       if(jsonMsg.train && jsonMsg.train.upload) {
         let files = [];
         jsonMsg.train.upload.forEach(function(val, i) {
@@ -19,7 +19,12 @@ wss.on('connection', function connection(ws) {
           files.push(file);
         });
         //console.log('message: files=<', files,'>');
-        addFiles2IpfsStorage(files);
+        addFiles2IpfsStorage(files,ws);
+      }
+      if(jsonMsg.tts && jsonMsg.tts.down) {
+        let file = jsonMsg.tts.down;
+        console.log('message: file=<', file,'>');
+        getFiles4IpfsStorage(file,ws);
       }
       
     } catch(e) {
@@ -68,7 +73,7 @@ ipfs.id(function (err, identity) {
 });
 
 
-function addFiles2IpfsStorage(files) {
+function addFiles2IpfsStorage(files,ws) {
   console.log('uploadSliceToIpfs::files=<',files,'>');
   if(files && files.length > 0) {
     ipfs.files.add(files,function(err, result){
@@ -78,22 +83,54 @@ function addFiles2IpfsStorage(files) {
       }
       console.log('uploadSliceToIpfs::result=<',result,'>');
       setTimeout(function () { 
-        sendBack2WS(result);
+        sendBack2WS(result,ws);
       },1);
     });
   } else {
     setTimeout(function () { 
-      sendBack2WS([]);
+      sendBack2WS([],ws);
     },1);
   }
 }
 
-function sendBack2WS(result) {
+function sendBack2WS(result,ws) {
   //console.log('sendBack2WS::wss.clients=<',wss.clients,'>');
+  /*
   wss.clients.forEach(function each(client) {
     console.log('sendBack2WS::client.readyState=<',client.readyState,'>');
     if (client.readyState) {
       client.send(JSON.stringify(result));
     };
   });
+  */
+  if (ws.readyState) {
+    ws.send(JSON.stringify(result));
+  };
+}
+
+
+function getFiles4IpfsStorage(file,ws) {
+  console.log('getFiles4IpfsStorage::file=<',file,'>');
+  if(file) {
+    ipfs.files.cat(file,function(err, result){
+      if (err) {
+        throw err;
+        process.exit();
+      }
+      console.log('getFiles4IpfsStorage::result=<',result,'>');
+      setTimeout(function () { 
+        sendBackFile2WS(result,ws);
+      },1);
+    });
+  } else {
+    setTimeout(function () { 
+      sendBackFile2WS({},ws);
+    },1);
+  }
+}
+
+function sendBackFile2WS(result,ws) {
+  if (ws.readyState) {
+    ws.send(JSON.stringify(result));
+  };
 }
