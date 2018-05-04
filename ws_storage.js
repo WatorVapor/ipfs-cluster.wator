45a8eaf636340cce77dc6ebdac61901c7b7ec8f1
Wator Vapor <watorvapor@gmail.com>
2018-05-04 12:23:44 +0900

    Update ws_storage.js

--- ws_storage.js
+++ ws_storage.js
@@ -62,7 +62,7 @@ const interval = setInterval(function ping() {
 
 var ipfsAPI = require('ipfs-api');
 //var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
-var ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'})
+var ipfs = ipfsAPI('localhost', '5002', {protocol: 'http'})
 console.log('ipfs=<',ipfs,'>');
 
 ipfs.id(function (err, identity) {
