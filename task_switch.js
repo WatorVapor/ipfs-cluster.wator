8ccf254d9b8d0a47a8cdb7d56692cdd667978f13
Wator Vapor <watorvapor@gmail.com>
2018-05-24 10:49:16 +0900

    Update task_switch.js

--- task_switch.js
+++ task_switch.js
@@ -13,42 +13,17 @@ ipfs.id( (err, identity) => {
 });
 
 
-const redis = require("redis");
-let pub = redis.createClient();
-let sub = redis.createClient();
-
-pub.on("ready", (err) => {
-  console.log('pub err=<',err,'>');
-});
-sub.on("ready", (err) => {
-  console.log('sub err=<',err,'>');
-});
-
-
-
-
-const topic = 'fruit-of-the-day';
-
+const watchTopic = 'wai-ipfs-task-switch-finnished';
 
 const receiveMsg = (msg) => {
   console.log('receiveMsg msg=<',msg,'>');
   //console.trace();
 }
-ipfs.pubsub.subscribe(topic, receiveMsg,(err) => {
+ipfs.pubsub.subscribe(watchTopic, receiveMsg,(err) => {
   if (err) {
     throw err
   }
-  console.log('subscribe topic=<',topic,'>');
+  console.log('subscribe watchTopic=<',watchTopic,'>');
 });
 
-setTimeout(function(){
-  const msg = new Buffer('banana');
-  ipfs.pubsub.publish(topic, msg, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log('sented msg=<',msg,'>');
-  });
-},1000);
-
-
+const broadcastTopic = 'wai-ipfs-task-switch-created';
