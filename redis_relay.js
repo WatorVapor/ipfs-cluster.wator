e62a8cf56c96889888c73826d1f370d2182a75a7
Wator Vapor <watorvapor@gmail.com>
2018-05-24 10:04:25 +0900

    Update redis_relay.js

--- redis_relay.js
+++ redis_relay.js
@@ -1,6 +1,6 @@
 const ipfsAPI = require('ipfs-api');
-//const ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001');
-const ipfs = ipfsAPI('localhost', '5002', {protocol: 'http'})
+const ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5002');
+//const ipfs = ipfsAPI('localhost', '5002', {protocol: 'http'})
 //const ipfs = ipfsAPI('ipfs.wator.xyz', '443', {protocol: 'https'})
 //console.log('ipfs=<',ipfs,'>');
 
