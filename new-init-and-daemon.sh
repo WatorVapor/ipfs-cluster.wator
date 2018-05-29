432c5a536e5e089b129901a53999ede352cae53c
Wator Vapor <watorvapor@gmail.com>
2018-05-29 22:41:24 +0900

    block

--- new-init-and-daemon.sh
+++ new-init-and-daemon.sh
@@ -1,5 +1,20 @@
-#!/bin/bash
-Storage=`pwd`/storage-ipfs
-export IPFS_PATH=${Storage}
-env
-go-ipfs/ipfs init
+#! /usr/bin/env bash
+
+set -e
+
+if [ -n "$IPFS_PATH" ]; then
+  echo "Using $IPFS_PATH as IPFS repository"
+else
+  echo "You need to set IPFS_PATH environment variable to use this script"
+  exit 1
+fi
+
+# Initialize the repo but ignore if error if it already exists
+# This can be the case when we restart a container without stopping/removing it
+node src/cli/bin.js init || true
+
+if [ -n "$IPFS_API_HOST" ]; then
+  sed -i.bak "s/127.0.0.1/$IPFS_API_HOST/g" $IPFS_PATH/config
+fi
+
+
