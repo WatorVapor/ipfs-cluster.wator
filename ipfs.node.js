818b5bb36ebb4283a01931eef940c433d890e56a
Wator Vapor <watorvapor@gmail.com>
2018-05-29 23:17:17 +0900

    Update ipfs.node.js

--- ipfs.node.js
+++ ipfs.node.js
@@ -1,6 +1,17 @@
 const IPFS = require('./src/index.js');
-const node = new IPFS({});
+const option = {
+  EXPERIMENTAL:{
+    pubsub:true,
+    sharding:true,
+    dht:true
+  }
+};
+const node = new IPFS();
 node.on('error', error => {
   console.error(error.message)
 });
-console.log('node=<',node,'>');
+
+node.on('ready', () => {
+  console.log('node=<',node,'>');
+});
+
